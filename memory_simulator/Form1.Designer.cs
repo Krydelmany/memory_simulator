7544d7423addc970bbf9c2d440546c6ba36e2a9d
Giovani Claro Moraes <giclamoraes@gmail.com>
2024-04-13 17:37:25 -0300

    Add project files.

---
+++ memory_simulator/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace memory_simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbMemoriaPrincipal = new System.Windows.Forms.GroupBox();
+            this.btnGerarNumeros = new System.Windows.Forms.Button();
+            this.btnAlternarVerificacao = new System.Windows.Forms.Button();
+            this.tbTimeStep = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gpListaAcesso = new System.Windows.Forms.GroupBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.btnLimparLista = new System.Windows.Forms.Button();
+            this.Acesso_Constante = new System.Windows.Forms.Button();
+            this.btnListaAcesso = new System.Windows.Forms.Button();
+            this.gpListaAcesso.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbMemoriaPrincipal
+            // 
+            this.gbMemoriaPrincipal.Font = new System.Drawing.Font("Segoe UI Emoji", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbMemoriaPrincipal.Location = new System.Drawing.Point(12, 12);
+            this.gbMemoriaPrincipal.Name = "gbMemoriaPrincipal";
+            this.gbMemoriaPrincipal.Size = new System.Drawing.Size(528, 267);
+            this.gbMemoriaPrincipal.TabIndex = 0;
+            this.gbMemoriaPrincipal.TabStop = false;
+            this.gbMemoriaPrincipal.Text = "Memoria Principal";
+            // 
+            // btnGerarNumeros
+            // 
+            this.btnGerarNumeros.Location = new System.Drawing.Point(94, 303);
+            this.btnGerarNumeros.Name = "btnGerarNumeros";
+            this.btnGerarNumeros.Size = new System.Drawing.Size(97, 23);
+            this.btnGerarNumeros.TabIndex = 1;
+            this.btnGerarNumeros.Text = "Preencher MP";
+            this.btnGerarNumeros.UseVisualStyleBackColor = true;
+            this.btnGerarNumeros.Click += new System.EventHandler(this.btnGerarNumeros_Click);
+            // 
+            // btnAlternarVerificacao
+            // 
+            this.btnAlternarVerificacao.Location = new System.Drawing.Point(171, 387);
+            this.btnAlternarVerificacao.Name = "btnAlternarVerificacao";
+            this.btnAlternarVerificacao.Size = new System.Drawing.Size(97, 23);
+            this.btnAlternarVerificacao.TabIndex = 2;
+            this.btnAlternarVerificacao.Text = "Acessar Lista";
+            this.btnAlternarVerificacao.UseVisualStyleBackColor = true;
+            this.btnAlternarVerificacao.Click += new System.EventHandler(this.btnAlternarVerificacao_Click);
+            // 
+            // tbTimeStep
+            // 
+            this.tbTimeStep.Location = new System.Drawing.Point(94, 389);
+            this.tbTimeStep.Name = "tbTimeStep";
+            this.tbTimeStep.Size = new System.Drawing.Size(75, 20);
+            this.tbTimeStep.TabIndex = 3;
+            this.tbTimeStep.Text = "50";
+            this.tbTimeStep.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbTimeStep.TextChanged += new System.EventHandler(this.tbTimeStep_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "TimeStep (ms)";
+            // 
+            // gpListaAcesso
+            // 
+            this.gpListaAcesso.Controls.Add(this.checkedListBox1);
+            this.gpListaAcesso.Location = new System.Drawing.Point(566, 13);
+            this.gpListaAcesso.Name = "gpListaAcesso";
+            this.gpListaAcesso.Size = new System.Drawing.Size(191, 266);
+            this.gpListaAcesso.TabIndex = 6;
+            this.gpListaAcesso.TabStop = false;
+            this.gpListaAcesso.Text = "Lista de Acesso";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(24, 31);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(146, 214);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // btnLimparLista
+            // 
+            this.btnLimparLista.Location = new System.Drawing.Point(197, 303);
+            this.btnLimparLista.Name = "btnLimparLista";
+            this.btnLimparLista.Size = new System.Drawing.Size(75, 23);
+            this.btnLimparLista.TabIndex = 7;
+            this.btnLimparLista.Text = "Limpar Lista";
+            this.btnLimparLista.UseVisualStyleBackColor = true;
+            this.btnLimparLista.Click += new System.EventHandler(this.btnLimparLista_Click);
+            // 
+            // Acesso_Constante
+            // 
+            this.Acesso_Constante.Location = new System.Drawing.Point(94, 415);
+            this.Acesso_Constante.Name = "Acesso_Constante";
+            this.Acesso_Constante.Size = new System.Drawing.Size(174, 23);
+            this.Acesso_Constante.TabIndex = 8;
+            this.Acesso_Constante.Text = "Acesso Constante";
+            this.Acesso_Constante.UseVisualStyleBackColor = true;
+            this.Acesso_Constante.Click += new System.EventHandler(this.Acesso_Constante_Click);
+            // 
+            // btnListaAcesso
+            // 
+            this.btnListaAcesso.Location = new System.Drawing.Point(94, 332);
+            this.btnListaAcesso.Name = "btnListaAcesso";
+            this.btnListaAcesso.Size = new System.Drawing.Size(174, 23);
+            this.btnListaAcesso.TabIndex = 9;
+            this.btnListaAcesso.Text = "Criar Lista de Acesso";
+            this.btnListaAcesso.UseVisualStyleBackColor = true;
+            this.btnListaAcesso.Click += new System.EventHandler(this.btnListaAcesso_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnListaAcesso);
+            this.Controls.Add(this.Acesso_Constante);
+            this.Controls.Add(this.btnLimparLista);
+            this.Controls.Add(this.gpListaAcesso);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTimeStep);
+            this.Controls.Add(this.btnAlternarVerificacao);
+            this.Controls.Add(this.btnGerarNumeros);
+            this.Controls.Add(this.gbMemoriaPrincipal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gpListaAcesso.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void tbTimeStep_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbMemoriaPrincipal;
+        private System.Windows.Forms.Button btnGerarNumeros;
+        private System.Windows.Forms.Button btnAlternarVerificacao;
+        private System.Windows.Forms.TextBox tbTimeStep;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gpListaAcesso;
+        private System.Windows.Forms.Button btnLimparLista;
+        private System.Windows.Forms.Button Acesso_Constante;
+        private System.Windows.Forms.Button btnListaAcesso;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ memory_simulator/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memory_simulator
+{
+    public partial class Form1 : Form
+    {
+        private const int Linhas = 8;
+        private const int Colunas = 16;
+        private Label[,] matrizLabels;
+        public Form1()
+        {
+            InitializeComponent();
+            CriarMatrizLabels();
+        }
+
+
+        private List<int> valoresAleatorios = new List<int>();
+        private void btnGerarNumeros_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < Linhas; i++)
+            {
+                for (int j = 0; j < Colunas; j++)
+                {
+                    int valorAleatorio = random.Next(1000); // Gera um número aleatório entre 0 e 999
+                    matrizLabels[i, j].Text = valorAleatorio.ToString(); // Atribui o valor aleatório ao Texto do Label
+                    valoresAleatorios.Add(valorAleatorio);
+                }
+            }
+        }
+
+        private void btnLimparLista_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Linhas; i++)
+            {
+                for (int j = 0; j < Colunas; j++)
+                {
+                    matrizLabels[i, j].Text = "0"; // Atribui o valor aleatório ao Texto do Label
+                }
+            }
+        }
+
+
+        private void btnListaAcesso_Click(object sender, EventArgs e)
+        {
+            // Limpa os itens existentes na CheckedListBox
+
+            // Chame a função para gerar os valores
+            btnGerarNumeros_Click(sender, e);
+
+            Random random = new Random();
+            List<int> valoresSelecionados = new List<int>();
+
+            while (valoresSelecionados.Count < 16)
+            {
+                int indiceAleatorio = random.Next(valoresAleatorios.Count);
+                int valorSelecionado = valoresAleatorios[indiceAleatorio];
+
+                // Verifica se o valor já foi selecionado antes de adicionar
+                if (!valoresSelecionados.Contains(valorSelecionado))
+                {
+                    valoresSelecionados.Add(valorSelecionado);
+                    checkedListBox1.Items.Add(valorSelecionado);
+                }
+            }
+        }
+
+
+        private int ultimaColunaVerificada = -1; // Armazena a última coluna verificada
+
+        // Evento de clique do botão para alternar a verificação dos Labels
+
+        private async void btnAlternarVerificacao_Click(object sender, EventArgs e)
+        {
+
+            int TimeStep = 100;
+            if (int.TryParse(tbTimeStep.Text, out int valorTempo))
+            {
+                TimeStep = valorTempo;
+            }
+
+            // Limpa a cor de fundo de todos os Labels
+            int proximaColunaVerificada = (ultimaColunaVerificada + 1) % Linhas ;
+            // Itera sobre todas as colunas
+            for (int j = 0; j < Linhas; j++)
+            {
+                // Define a cor de fundo do próximo Label em cada coluna como verde
+                for (int i = 0; i < Colunas; i++)
+                {
+                    matrizLabels[j, i].BackColor = Color.LightGreen;
+                    await Task.Delay(TimeStep); // Aguarda um atraso de 100 milissegundos antes de limpar as cores
+                    matrizLabels[j, proximaColunaVerificada].BackColor = Color.Transparent; // Limpa a cor de fundo após o atraso
+                    LimparCoresDeFundo(); // Limpa a cor de fundo após o atraso
+                }
+
+            }
+            ultimaColunaVerificada = proximaColunaVerificada;
+        }
+
+        bool loop_verif = false; // Inicialmente, o loop não está ativo
+        private bool botaoClicado = false;
+        private async void Acesso_Constante_Click(object sender, EventArgs e)
+        {
+            // Alterna o estado do botão
+            botaoClicado = !botaoClicado;
+
+            // Define a cor do botão com base no estado atual
+            if (botaoClicado)
+            {
+                Acesso_Constante.BackColor = Color.LightGreen; // Define a cor para verde
+            }
+            else
+            {
+                Acesso_Constante.BackColor = SystemColors.Control; // Retorna a cor ao normal
+            }
+
+            // Inverte o valor de loop_verif
+            loop_verif = !loop_verif;
+
+            // Se loop_verif agora for falso, significa que o usuário deseja parar o loop
+            if (!loop_verif)
+            {
+                return;
+            }
+
+            int TimeStep = 100; // Tempo de atraso padrão
+            if (int.TryParse(tbTimeStep.Text, out int valorTempo))
+            {
+                TimeStep = valorTempo;
+            }
+
+            // Limpa a cor de fundo de todos os Labels
+            LimparCoresDeFundo();
+
+            int proximaColunaVerificada = (ultimaColunaVerificada + 1) % Colunas;
+            while (loop_verif)
+            {
+                for (int j = 0; j < Linhas; j++)
+                {
+                    // Define a cor de fundo do próximo Label em cada coluna como verde
+                    for (int i = 0; i < Colunas; i++)
+                    {
+                        matrizLabels[j, i].BackColor = Color.LightGreen;
+                        await Task.Delay(TimeStep); // Aguarda um atraso de acordo com o valor de TimeStep
+                        matrizLabels[j, i].BackColor = Color.Transparent;
+                    }
+                }
+            }
+        }
+
+
+
+
+        // Método para limpar a cor de fundo de todos os Labels
+        private void LimparCoresDeFundo()
+        {
+            for (int i = 0; i < Colunas; i++)
+            {
+                for (int j = 0; j < Linhas; j++)
+                {
+                    matrizLabels[j, i].BackColor = Color.Transparent;
+                }
+            }
+        }
+
+        private void CriarMatrizLabels()
+        {
+            // Inicializar a matriz de labels com o tamanho correto
+            matrizLabels = new Label[Linhas, Colunas];
+
+            // Posicionamento inicial dos labels
+            int left = 10;
+            int top = 10;
+            int deslocamentoVertical = 20; // Valor de deslocamento vertical
+            // Criar e posicionar cada label na matriz
+            for (int i = 0; i < Linhas; i++)
+            {
+                for (int j = 0; j < Colunas; j++)
+                {
+                    // Criar um novo label
+                    Label label = new Label();
+                    label.AutoSize = true;
+                    label.Left = left;
+                    label.Top = top;
+                    label.BorderStyle = BorderStyle.Fixed3D;
+                    label.TextAlign = ContentAlignment.MiddleCenter;
+                    label.Text = $" 0 "; // Texto inicial, pode ser alterado conforme necessário
+                    // Adicionar o label ao formulário  
+                    Controls.Add(label);
+                    // Adicionar o label à matriz
+                    matrizLabels[i, j] = label;
+                    // Ajustar a posição para o próximo label
+                    left += label.Width + 10; // Adicione uma margem de 5 pixels entre os labels
+                    gbMemoriaPrincipal.Controls.Add(label);
+                    label.Top += deslocamentoVertical;
+                }
+                // Resetar a posição horizontal para a próxima linha
+                left = 10;
+                // Ajustar a posição vertical para a próxima linha
+                top += matrizLabels[0, 0].Height + 10; // Adicione uma margem de 5 pixels entre as linhas
+            }
+        }
+    }
+}
