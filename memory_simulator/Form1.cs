5cd0d29e21d3c680acc8b6c1e04273625dd37626
Giovani Claro Moraes <giclamoraes@gmail.com>
2024-04-13 19:14:20 -0300

    Algumas alterações e adições para o projeto

--- memory_simulator/Form1.cs
+++ memory_simulator/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Printing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,7 +21,7 @@ namespace memory_simulator
         public Form1()
         {
             InitializeComponent();
-            CriarMatrizLabels();
+            MatrizLabels();
         }
 
 
@@ -46,7 +47,7 @@ namespace memory_simulator
             {
                 for (int j = 0; j < Colunas; j++)
                 {
-                    matrizLabels[i, j].Text = "0"; // Atribui o valor aleatório ao Texto do Label
+                    matrizLabels[i, j].Text = "       "; // Atribui o valor aleatório ao Texto do Label
                 }
             }
         }
@@ -55,7 +56,8 @@ namespace memory_simulator
         private void btnListaAcesso_Click(object sender, EventArgs e)
         {
             // Limpa os itens existentes na CheckedListBox
-
+            checkedListBox1.Items.Clear();
+            valoresAleatorios.Clear();
             // Chame a função para gerar os valores
             btnGerarNumeros_Click(sender, e);
 
@@ -83,6 +85,8 @@ namespace memory_simulator
 
         private async void btnAlternarVerificacao_Click(object sender, EventArgs e)
         {
+            // Define o índice selecionado da CheckedListBox como 0
+            checkedListBox1.SelectedIndex = 0;
 
             int TimeStep = 100;
             if (int.TryParse(tbTimeStep.Text, out int valorTempo))
@@ -91,22 +95,29 @@ namespace memory_simulator
             }
 
             // Limpa a cor de fundo de todos os Labels
-            int proximaColunaVerificada = (ultimaColunaVerificada + 1) % Linhas ;
+            int proximaColunaVerificada = (ultimaColunaVerificada + 1) % Linhas;
             // Itera sobre todas as colunas
             for (int j = 0; j < Linhas; j++)
             {
                 // Define a cor de fundo do próximo Label em cada coluna como verde
                 for (int i = 0; i < Colunas; i++)
                 {
-                    matrizLabels[j, i].BackColor = Color.LightGreen;
+                    matrizLabels[j, i].BackColor = Color.Yellow;
                     await Task.Delay(TimeStep); // Aguarda um atraso de 100 milissegundos antes de limpar as cores
                     matrizLabels[j, proximaColunaVerificada].BackColor = Color.Transparent; // Limpa a cor de fundo após o atraso
                     LimparCoresDeFundo(); // Limpa a cor de fundo após o atraso
-                }
 
+                    // Verifica se o item na primeira CheckedListBox corresponde ao valor atual
+                    if (checkedListBox1.Items.Count > 0 && Convert.ToInt32(checkedListBox1.Items[0]) == proximaColunaVerificada)
+                    {
+                        matrizLabels[j, proximaColunaVerificada].BackColor = Color.Red; // Define a cor vermelha se corresponder
+                    }
+                }
             }
             ultimaColunaVerificada = proximaColunaVerificada;
         }
+
+
 
         bool loop_verif = false; // Inicialmente, o loop não está ativo
         private bool botaoClicado = false;
@@ -174,7 +185,7 @@ namespace memory_simulator
             }
         }
 
-        private void CriarMatrizLabels()
+        private void MatrizLabels()
         {
             // Inicializar a matriz de labels com o tamanho correto
             matrizLabels = new Label[Linhas, Colunas];
@@ -195,7 +206,7 @@ namespace memory_simulator
                     label.Top = top;
                     label.BorderStyle = BorderStyle.Fixed3D;
                     label.TextAlign = ContentAlignment.MiddleCenter;
-                    label.Text = $" 0 "; // Texto inicial, pode ser alterado conforme necessário
+                    label.Text = $"       "; // Texto inicial, pode ser alterado conforme necessário
                     // Adicionar o label ao formulário  
                     Controls.Add(label);
                     // Adicionar o label à matriz
